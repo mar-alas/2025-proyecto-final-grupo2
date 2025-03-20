20955de3c6e014ec6145ed953b43b677786bc72a
mar-alas <m.alas@uniandes.edu.co>
2025-03-20 10:40:14 -0700

    Agregando estructura de carpetas para todos los microservicios, los dockerfiles, y el docker-compose, ping en todos los servicios, y una prueba unitaria del ping, github action para correr las pruebas del microervicio gestor_compras

---
+++ backend/ventas_servicio/gestor_ventas/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from seedwork_compartido.aplicacion.lectura.ping import ping_bp
+
+URL_PREFIX = '/api/v1/ventas/gestor_ventas'
+
+app = Flask(__name__)
+app.register_blueprint(ping_bp, url_prefix=URL_PREFIX)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=3008)
