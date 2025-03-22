eea725f5ff75ceba34440e7d96b62dd9886b4864
Jhon Freddy Puentes Nuñez <jhonfredypuentesnunez@MacBook-Pro-de-Usuario.local>
2025-03-22 16:57:47 -0500

    feat: Implementacion de servicio home para responder a healh checks de GKE.

--- backend/ventas_servicio/procesador_video/app.py
+++ backend/ventas_servicio/procesador_video/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from seedwork_compartido.aplicacion.lectura.ping import ping_bp
+from aplicacion.lecturas.home import home_bp
 
 URL_PREFIX = '/api/v1/ventas/procesador_video'
+URL_HOME = "/"
 
 app = Flask(__name__)
 app.register_blueprint(ping_bp, url_prefix=URL_PREFIX)
+app.register_blueprint(home_bp, url_prefix=URL_HOME)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=3010)
