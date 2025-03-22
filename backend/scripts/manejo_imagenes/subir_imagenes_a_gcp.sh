7c273b4441e7111915ef69811b935734887d4b2b
Jhon Freddy Puentes Nuñez <jhonfredypuentesnunez@MacBook-Pro-de-Usuario.local>
2025-03-22 18:25:43 -0500

    build: Implementacion completa de carga de imagenes a GCP.

--- backend/scripts/manejo_imagenes/subir_imagenes_a_gcp.sh
+++ backend/scripts/manejo_imagenes/subir_imagenes_a_gcp.sh
@@ -1,4 +1,17 @@
 echo "Cargando imagen [compras_servicio/gestor_compras] a GCP..."
+
 sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/compras_servicio/gestor_compras:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/inventario_servicio/gestor_productos:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/inventario_servicio/gestor_proveedores:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/inventario_servicio/gestor_stock:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/logistica_servicio/generador_reportes:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/logistica_servicio/generador_rutas_entrega:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/logistica_servicio/gestor_entregas:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/seguridad_servicio/gestor_usuarios:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/ventas_servicio/generador_reportes:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/ventas_servicio/gestor_ventas:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/ventas_servicio/procesador_pedidos:latest
+sudo docker push us-central1-docker.pkg.dev/proyecto-final-2-454403/repositorio-imagenes-docker/ventas_servicio/procesador_video:latest
+
 echo "Proceso de cargue de imagen [compras_servicio/gestor_compras] terminado."
 
