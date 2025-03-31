cd91913c1b269c61f57b26dcdfb7bf6ca27531c8
mar-alas <m.alas@uniandes.edu.co>
2025-03-31 13:32:16 -0700

    Agregando feature proveedores

--- backend/inventario_servicio/gestor_proveedores/app.py
+++ backend/inventario_servicio/gestor_proveedores/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from seedwork_compartido.aplicacion.lectura.ping import ping_bp
+from aplicacion.lecturas.proveedores import proveedores_lectura
 from aplicacion.lecturas.home import home_bp
+from aplicacion.escrituras.proveedores import proveedores_escritura
 
 URL_PREFIX = '/api/v1/inventario/gestor_proveedores'
 URL_HOME = "/"
@@ -8,6 +10,8 @@ URL_HOME = "/"
 app = Flask(__name__)
 app.register_blueprint(ping_bp, url_prefix=URL_PREFIX)
 app.register_blueprint(home_bp, url_prefix=URL_HOME)
+app.register_blueprint(proveedores_escritura, url_prefix=URL_PREFIX)
+app.register_blueprint(proveedores_lectura, url_prefix=URL_PREFIX)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=3003)
---
+++ backend/inventario_servicio/gestor_proveedores/infraestructura/schema.py
@@ -0,0 +1,19 @@
+from marshmallow import Schema, fields
+
+class ProveedorInputSchema(Schema):
+    nombre = fields.String(required=True, max_length=100)
+    email = fields.Email(required=True, max_length=100)
+    numero_contacto = fields.String(required=True, max_length=20)
+    pais = fields.String(required=True, max_length=50)
+    caracteristicas = fields.String(max_length=500)
+    condiciones_comerciales_tributarias = fields.String(max_length=1000)
+
+class ProveedorResponseSchema(Schema):
+    id = fields.Integer()
+    nombre = fields.String()
+    email = fields.String()
+    numero_contacto = fields.String()
+    pais = fields.String()
+    caracteristicas = fields.String()
+    condiciones_comerciales_tributarias = fields.String()
+    fecha_registro = fields.DateTime()
