e3791c92225818f255a6269d6f37c2a22c2cc3ae
Jhon Freddy Puentes Nuñez <jhonfredypuentesnunez@192.168.10.9>
2025-04-02 07:36:59 -0500

    feat: Entidad User necesaria para regisrar usuarios en BD.

---
+++ backend/seguridad_servicio/gestor_usuarios/dominio/user.py
@@ -0,0 +1,24 @@
+import uuid
+from infraestructura.database import db
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    uuid = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)  # Se almacenará el hash
+    role = db.Column(db.String(50), nullable=True)  # Opcional
+    country = db.Column(db.String(100), nullable=True)  # Opcional
+    city = db.Column(db.String(100), nullable=True)  # Opcional
+    address = db.Column(db.String(255), nullable=True)  # Opcional
+
+    def __init__(self, name, email, password, role=None, country=None, city=None, address=None):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.role = role
+        self.country = country
+        self.city = city
+        self.address = address
